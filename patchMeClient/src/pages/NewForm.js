e030522a566545512554241e92fdb2dfac645aa1
rs222kn <rs222kn@student.lnu.se>
2016-10-20 21:11:17 +0200

    formControl functions implemented

--- patchMeClient/src/pages/NewForm.js
+++ patchMeClient/src/pages/NewForm.js
@@ -1,10 +1,12 @@
 'use strict';
 import React, { Component } from 'react';
-import co from 'co';
 import InputOptionsList from '../components/InputOptionsList';
-
+import update from 'react-addons-update';
 import FormControl from '../components/FormControl';
-import { InputShortText, InputLongText } from '../components/InputOptions';
+import FormHeader from '../components/FormHeader';
+import { getUniqId } from '../utils/id';
+import { getPositionOfElementInArray, moveElementForwardInArray, moveElementBackwardsInArray } from '../utils/arrayHelper';
+import { InputHeader, InputShortText, InputLongText } from '../components/InputOptions';
 
 class NewForm extends Component {
   constructor(props){
@@ -12,50 +14,106 @@ class NewForm extends Component {
 
     this.state = {
       displayList: false,
-      elements: []
+      elements: [],
+      focusElement: null
     };
   }
 
-  displayFormOptions(){
-    if(this.state.displayList){
-      return this.setState({ displayList: false });
-    }
-    this.setState({ displayList: true });
+  toggleDisplayFormOptions(){
+    this.removeElementFocus();
+    return this.state.displayList ?
+      this.setState({ displayList: false }) :
+      this.setState({ displayList: true });
   }
 
-  addItem(){
+  setElementFocus(e){
+    this.removeElementFocus();
+    e.style.boxShadow = '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)';
+    e.style.marginTop = '8px';
+    e.style.marginBottom = '8px';
+    this.setState({focusElement: e});
+  }
 
+  removeElementFocus(){
+    if(this.state.focusElement){
+      this.state.focusElement.style.boxShadow = '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)';
+      this.state.focusElement.style.marginTop = 'initial';
+      this.state.focusElement.style.marginBottom = 'initial';
+    }
+  }
+
+  updateData(id, value){
+    const element = getPositionOfElementInArray(this.state.elements, id);
+    this.setState({
+      elements: update(this.state.elements, {[element._pos]: {description: {$set: value}}})
+    });
+  }
+
+  getUniqFormControlId(){
+    return getUniqId(this.state.elements.map(e => e.id));
+  }
+
+  removeItem(id){
+    const element = getPositionOfElementInArray(this.state.elements, id);
+    this.setState({
+      elements: update(this.state.elements, {$splice: [[element._pos, 1]]})
+    });
+  }
+
+  moveElementUp(id){
+    const element = getPositionOfElementInArray(this.state.elements, id);
+    const elements = moveElementForwardInArray(this.state.elements, element);
+    this.setState({elements: elements});
+  }
+
+  moveElementDown(id){
+    const element = getPositionOfElementInArray(this.state.elements, id);
+    const elements = moveElementBackwardsInArray(this.state.elements, element);
+    this.setState({elements: elements});
+  }
+
+  addItem(element, shell){
+    this.toggleDisplayFormOptions();
+    const id = this.getUniqFormControlId();
+
+    let props = JSON.parse(JSON.stringify(shell));
+    props.id = id;
+    props.element = React.cloneElement(<FormControl/>, {
+      fcId: id,
+      update: this.updateData.bind(this),
+      remove: this.removeItem.bind(this),
+      moveUp: this.moveElementUp.bind(this),
+      moveDown: this.moveElementDown.bind(this),
+      setElementFocus: this.setElementFocus.bind(this),
+      Element: element
+    });
+
+    this.setState({elements: update(this.state.elements, { $push: [props] })});
   }
 
   render() {
     return (
       <div>
-        <section className=" is-small is-bold">
-          <div className="hero-body">
-            <div className="container has-text-centered">
-              <h1 className="title">
-                <input className="input is-large formTitle" type="text" placeholder="Untitled form" />
+        <FormHeader />
+        <div className="container has-text-centered formContentWrap">
 
-              </h1>
-              <h2 className="subtitle">
-                <textarea className="textarea formDescription" placeholder="From description"></textarea>
-              </h2>
-            </div>
-          </div>
-        </section>
-        <div className="container has-text-centered">
-          <FormControl Element={InputLongText}/>
-
+          <InputHeader/>
+          {(() => {
+            if(this.state.elements.length !== 0){
+              return (this.state.elements
+                .map((e, i ) => {
+                  return React.cloneElement(e.element, { key: e.id });
+              }));
+            }
+          })()}
           <nav className="level addFormOptionLevel">
             <p className="level-item has-text-centered">
-              <button onClick={this.displayFormOptions.bind(this)} className="button is-success addFormControlButton" >
+              <button onClick={this.toggleDisplayFormOptions.bind(this)} className="button is-success addFormControlButton" >
                 <span className="icon">
                   <i className="fa fa-plus-circle" aria-hidden="true"></i>
                 </span>
               </button>
             </p>
-            <br />
-
           </nav>
           <InputOptionsList onItemPress={this.addItem.bind(this)} display={this.state.displayList} />
         </div>
